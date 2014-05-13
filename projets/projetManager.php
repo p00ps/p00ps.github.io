3d4f46ef9ad0de44fb8deee7d57b08bbd8f8ab33
p00ps <khuu.sylvia@gmail.com>
2014-05-13 18:17:42 +0200

    projetmanager/php ?

---
+++ projets/projetManager.php
@@ -0,0 +1,68 @@
+<?php
+
+switch ($_GET['nomProjet']) {
+	case 'android':
+		displayAndroidProjet();
+		break;
+	case 'php':
+		displayPhpProjet();
+		break;
+	case 'cms':
+		displayCmsProjet();
+		break;
+	default:
+		break;
+}
+
+function displayAndroidProjet(){
+echo'
+    <div class="large-12 columns" role="content">
+      <article>
+        <h3>projet perso d\' application android</h3>
+        <div class="row">
+          <div class="large-8 columns">
+            <p>top secret super difficile, arrachage de cheveux garanti pour noob</p>
+            <p>et patati et pastrami</p>
+          </div>
+          <div class="large-4 columns">
+            <img src="http://placehold.it/400x240&text=[img]" />
+          </div>
+        </div>
+      </article>';
+}
+
+function displayPhpProjet(){
+echo'
+    <div class="large-12 columns" role="content">
+      <article>
+        <h3>Site web dynamique recensant mes recettes de cuisine franco-asiatique</h3>
+        <div class="row">
+          <div class="large-8 columns">
+            <p>peusheupeuh.</p>
+            <p>et patati et pastrami</p>
+          </div>
+          <div class="large-4 columns">
+            <img src="http://placehold.it/400x240&text=[img]" />
+          </div>
+        </div>
+      </article>';
+}
+
+function displayCmsProjet(){
+echo'
+    <div class="large-12 columns" role="content">
+      <article>
+        <h3>Création site marchand vente bijoux fantaisie</h3>
+        <div class="row">
+          <div class="large-8 columns">
+            <p>les fantaisies de mon amie</p>
+            <p>et patati et pastrami</p>
+          </div>
+          <div class="large-4 columns">
+            <img src="http://placehold.it/400x240&text=[img]" />
+          </div>
+        </div>
+      </article>';
+}
+
+?>
