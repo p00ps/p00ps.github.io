0cd3c2dcffef3897ad85b0ab0457f5fa90bb5e47
p00ps <khuu.sylvia@gmail.com>
2014-09-14 16:33:42 +0200

    adding addson js/test jquery

---
+++ js/addson.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){    
+
+	var c = $("thumbnails").children().find("a");
+	c.click(function(){
+		filtre();
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+function filtre(){
+	alert ("toto");
+
+
+}
