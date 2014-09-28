f302fddd171f1e5d474ff8c117655dd0d32c6878
p00ps <khuu.sylvia@gmail.com>
2014-09-28 17:03:28 +0200

    maj 28/09 js + css

--- js/addson.js
+++ js/addson.js
@@ -8,14 +8,44 @@ $(document).ready(function(){
 
 	filtre(0);
 
-
+	repositionScroll();
 
 });
 
 
-function filtre(pId){
-	var article = $("#filtreconteneur").find("article");
-	article.css('display','none');
-	article.eq(pId).css('display','block');
+
+
+//------------------------------------------------------> repositionnement des ancres
+
+function repositionScroll(){
+
+	$('.side-nav li').find('a').on('click', function(evt){
+       // bloquer le comportement par défaut: on ne rechargera pas la page
+       evt.preventDefault(); 
+       // enregistre la valeur de l'attribut  href dans la variable target
+	var target = $(this).attr('href');
+       /* le sélecteur $(html, body) permet de corriger un bug sur chrome et safari (webkit) */
+	$('html, body')
+       // on arrête toutes les animations en cours 
+       .stop()
+       /* on fait maintenant l'animation vers le haut (scrollTop) vers notre ancre target */
+       .animate({scrollTop: $(target).offset().top}, 1000 );
+    });
+
+	$('.scrollverstop').click(function(e){
+		e.preventDefault();
+		$('html, body').stop().animate({scrollTop: 0}, 'fast');
+	});
 
 }
+
+//------------------------------------------------------> filtre projet
+
+function filtre(pId){
+	var article = $("#filtreconteneur").find("article");
+	article.eq(pId).stop().fadeIn(2000);
+	article.css('display','none');
+	article.eq(pId).css('display','block');
+}
+
+
